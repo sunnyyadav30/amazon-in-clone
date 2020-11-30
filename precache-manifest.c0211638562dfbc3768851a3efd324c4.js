f4f42d94b70a8e209ac8e280f4054583a2ed4596
sunnyyadav30 <sunnyyv30@gmail.com>
2020-11-30 16:03:12 +0530

    Updates

---
+++ precache-manifest.c0211638562dfbc3768851a3efd324c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98757b7d5f535a15cf7702de6a0e3ad3",
+    "url": "/amazon-in-clone/index.html"
+  },
+  {
+    "revision": "6e99569b438db79ba5dc",
+    "url": "/amazon-in-clone/static/css/main.3ee8010f.chunk.css"
+  },
+  {
+    "revision": "33729702fb15d3b37bdd",
+    "url": "/amazon-in-clone/static/js/2.1444ab08.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-in-clone/static/js/2.1444ab08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e99569b438db79ba5dc",
+    "url": "/amazon-in-clone/static/js/main.30cfd45e.chunk.js"
+  },
+  {
+    "revision": "e412c8c0669e9a6861e6",
+    "url": "/amazon-in-clone/static/js/runtime-main.1dbcd4b4.js"
+  }
+]);
